31d5f0db086800c73bb58349bd1ccb69f73d5cf3
OmarMeriwani <omar.sirwan@gmail.com>
2019-01-28 23:44:10 +0000

    Bank data - Finished

--- lab3/Bank.py
+++ lab3/Bank.py
@@ -1,19 +1,62 @@
 import numpy as np
 import sklearn
 import pandas as pd
-print(sklearn.__version__)
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import confusion_matrix
+import itertools
+from sklearn.model_selection import KFold, cross_val_score
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        t = "(%.2f)"%(cm[i, j])
+        #print t
+#         plt.text(j, i, t,
+#                  horizontalalignment="center",
+#                  color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.savefig('Bank_confusion_matrix.png')
+
+
+#Read sheet
 bank = pd.read_csv('bank-additional-full.csv', sep=';',header=0)
 print(bank.keys())
 print(bank.shape)
+#Remove Nulls
 bank.dropna()
 le = LabelEncoder()
+#Remove duration
 bank.pop('duration')
+#Convert categories into numerical values
 bank['job'] = le.fit_transform(bank['job'])
 bank['marital'] = le.fit_transform(bank['marital'])
 bank['education'] = le.fit_transform(bank['education'])
@@ -24,15 +67,45 @@ bank['contact'] = le.fit_transform(bank['contact'])
 bank['month'] = le.fit_transform(bank['month'])
 bank['day_of_week'] = le.fit_transform(bank['day_of_week'])
 bank['poutcome'] = le.fit_transform(bank['poutcome'])
-X = bank.values[:,0:19]
+#Create dummies
+df_dummies = pd.get_dummies(bank)
+#Remove Y_NO
+df_dummies.pop('y_no')
+print(df_dummies.head())
+#Create the histogram
+fig = plt.hist(df_dummies['y_yes'])
+plt.savefig('y_yes_histogram.png')
+#Get X and Y
+X = df_dummies.values[:,0:19]
 print(X)
-Y = bank.values[:, 19]
+Y = df_dummies.values[:, 19]
 print(Y)
-
-Y2 = [0 if a == 'no' else 1 for a in Y]
-
-x_train,x_test,y_train,y_test = train_test_split(X,Y2, test_size=0.3, random_state=100)
+#Split training and testing data
+x_train,x_test,y_train,y_test = train_test_split(X,Y, test_size=0.3, random_state=100)
+#Standardization
 scaler = StandardScaler()
-scaler.fit(X)
-X = scaler.transform(X)
-fig2 = plt.boxplot(X)
+scaler.fit(x_train)
+X = scaler.transform(x_train)
+X = scaler.transform(x_test)
+#Classifier fitting
+ann = MLPClassifier(activation='tanh', hidden_layer_sizes=(23, 23, 23), solver='lbfgs', random_state=100)
+ann.fit(x_train,y_train)
+
+#Prediction
+y_pred = ann.predict(x_test)
+
+#Confusion matrix
+cnf_matrix = confusion_matrix(y_test, y_pred)
+np.set_printoptions(precision=2)
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=range(len(set(y_test))), normalize = False,title='Confusion matrix')
+
+#Folds testing
+ann = MLPClassifier(activation='tanh', hidden_layer_sizes=(23, 23, 23), solver='lbfgs', random_state=100)
+k_fold = KFold(n_splits=10)
+for train_indices, test_indices in k_fold.split(X): # consider the first 40 examples
+    print('Train: %s | test: %s' % (train_indices, test_indices))
+    ann.fit(X[train_indices], Y[train_indices])
+    print('Fold test accuracy: {} %'.format(ann.score(X[train_indices], Y[train_indices])*100))
+
+#Feature importance
--- lab3/Readme.md
+++ lab3/Readme.md
@@ -6,11 +6,33 @@ After fitting the model on all the data except the last one, it appeared that th
 
 After applying the confusion matrix method to the predicted results and the original results, it appeared that true positive were more than true negatives, both false negatives and positive had very few cases.
 
+![alt text](confusion.png)
+
+Confusion matrix for SVM
+
+![alt text](confusion_dt.png)
+
+Confusion matrix for DT
+
 Fold test accuracy showed results varying between 62% and 100% and with average accuracy 94%. Best gamma value chosen was 1e-05 with C value 1000 for 97% accuracy, the same gama and C values were selected for other higher accuracy values.
 
 Similar results of confusion matrix were achieved via decision trees.
+
+![alt text](importances_dt.png)
+
+Feature importances for decision tree
+
 
 ## After standadization
 After doing the standardization, folds test gave higher accuracy values for SVM, the average accuracy value was 97%. C value was less, which refers to a better decision function performance. Gamma value was higher after doing standardization, which refers to less effect of single sample on the general performance.
 
 Decision tree has not been affected by the standardization.
+# Bank data analysis
+Null values were removed, as long as the unwanted columns, the data frame then was saved at a dummies object. 
+![alt text](y_yes_histogram.png)
+
+The data later were divided into test and training, and an Artificial Neural Network Classifier was used. Confusion matrix was applied to the results as follows:
+![alt text](bank_confusion_matrix.png)
+
+The accuracy during the 10 folds test has exceeded 99% for all folds.
+
