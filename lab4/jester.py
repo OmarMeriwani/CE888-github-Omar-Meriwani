fcb0886f0b23899721e13f843690bb9598ebaebd
OmarMeriwani <omar.sirwan@gmail.com>
2019-02-04 21:32:44 +0000

    Lab 4 adding prediciton and test methods

--- lab4/jester.py
+++ lab4/jester.py
@@ -28,7 +28,7 @@ randoms_range = len(user_preferences) - 1
 print(randoms_range)
 user_features_randoms = [random.randint(0,randoms_range) for i in range(0,count_of_randoms)]
 print(len(user_features_randoms))
-changed_values = [user_preferences[i] for i in user_features_randoms]
+users_changed_values = [user_preferences[i] for i in user_features_randoms]
 for i in user_features_randoms:
     user_preferences_array[i] = 99
 
@@ -39,15 +39,18 @@ randoms_range = len(joke_features) - 1
 print(randoms_range)
 joke_features_randoms = [random.randint(0,randoms_range) for i in range(0,count_of_randoms)]
 print(len(joke_features_randoms))
-changed_values = [joke_features[i] for i in joke_features_randoms]
+jokes_changed_values = [joke_features[i] for i in joke_features_randoms]
 for i in joke_features_randoms:
     joke_features_array[i] = 99
 
 print(joke_features_array)
-'''
-def predict_rating(user_id,item_id):
+
+
+def predict_rating(user_id, item_id):
+    """ Predict a rating given a user_id and an item_id.
+    """
     user_preference = latent_user_preferences[user_id]
-    item_preference = latent_joke_features[item_id]
+    item_preference = latent_item_features[item_id]
     return user_preference.dot(item_preference)
 
 
@@ -57,7 +60,33 @@ def train(user_id, item_id, rating, alpha=0.0001):
     err = (prediction_rating - rating);
     # print err
     user_pref_values = latent_user_preferences[user_id][:]
-    latent_user_preferences[user_id] -= alpha * err * latent_joke_features[item_id]
-    latent_joke_features[item_id] -= alpha * err * user_pref_values
+    latent_user_preferences[user_id] -= alpha * err * latent_item_features[item_id]
+    latent_item_features[item_id] -= alpha * err * user_pref_values
     return err
-'''
+
+
+def sgd(iterations=300000):
+    """ Iterate over all users and all items and train for
+        a certain number of iterations
+    """
+    for iteration in range(0, iterations):
+        error = []
+        for user_id in range(0, latent_user_preferences.shape[0]):
+            for item_id in range(0, latent_item_features.shape[0]):
+                rating = user_ratings[user_id][item_id]
+                if (not np.isnan(rating)):
+                    err = train(user_id, item_id, rating)
+                    error.append(err)
+        mse = (np.array(error) ** 2).mean()
+        if (iteration % 10000 == 0):
+            print
+            mse
+
+
+predictions = latent_user_preferences.dot(latent_item_features.T)
+print(predictions)
+
+values = [zip(user_ratings[i], predictions[i]) for i in range(0,predictions.shape[0])]
+comparison_data = pd.DataFrame(values)
+comparison_data.columns = data.columns
+comparison_data.applymap(lambda (x,y): "(%2.3f|%2.3f)"%(x,y))
