d7f401b3bf4bd868dac6e504485c319a6306c1a8
OmarMeriwani <omar.sirwan@gmail.com>
2019-02-21 00:35:35 +0000

    Project1 Initial Commit

---
+++ Project 1/preprocessing.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numba as np
+
+file1 = pd.read_csv('diabetes.csv')
+file1.dropna()
+
+
+file2 = pd.read_csv('hear-attack.csv')
+file2.dropna()
+
+
+file3 = pd.read_csv('Toddler Autism dataset July 2018.csv')
+file3.dropna()
